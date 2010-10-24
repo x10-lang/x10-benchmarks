40fd66a017a6efb91d8c87a26a89e3f2bd977bd2
Vijay Saraswat <vsaraswa@us.ibm.com>
2010-10-24 12:12:33 +0000

    Converting to an X10 project.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/benchmarks/trunk@17733 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ GLB/bin/Runner.java
@@ -0,0 +1,22 @@
+public interface Runner<T, Z>
+  extends x10.core.fun.Fun_0_2
+{
+    public static final x10.rtt.RuntimeType<Runner> _RTT = new x10.rtt.RuntimeType<Runner>(
+    /* base class */Runner.class, 
+    /* variances */ new x10.rtt.RuntimeType.Variance[] {x10.rtt.RuntimeType.Variance.INVARIANT, 
+    x10.rtt.RuntimeType.Variance.INVARIANT}
+    , /* parents */ new x10.rtt.Type[] {x10.rtt.Types.ANY, new x10.rtt.ParameterizedType(x10.core.fun.Fun_0_2._RTT, new x10.rtt.UnresolvedType(0), new x10.rtt.ParameterizedType(x10.
+      lang.
+      Reducible._RTT, new x10.rtt.UnresolvedType(1)), new x10.rtt.UnresolvedType(1))}
+    ) {public String typeName() {return "Runner";}};
+    
+        
+        
+        
+//#line 10
+void
+                      stats(
+                      final long time,
+                      final boolean verbose);
+    
+}
