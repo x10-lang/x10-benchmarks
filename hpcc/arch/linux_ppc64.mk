23581d12ab31039f14d393934440da3a1ae0d088
Benjamin Herta <bherta@us.ibm.com>
2011-06-28 19:10:32 +0000

    Makefile adjustments, to change lapi to pami, and add in some linux_ppc64 specific arguments
    git-svn-id: svn://svn.code.sf.net/p/x10/code/benchmarks/trunk@22357 a326200e-df2c-42e0-81fa-597e909af41d

--- hpcc/arch/linux_ppc64.mk
+++ hpcc/arch/linux_ppc64.mk
@@ -14,7 +14,7 @@ export POSTCOMPILE_NATIVE_LIB_FLAGS ?= -O3 -DNDEBUG
 ifeq ($(USE_ACML), 1)
 export BLAS_LIB		?= /opt/acml4.3.0/gfortran64/lib/libacml.a /usr/lib/gcc/x86_64-redhat-linux/4.1.1/libgfortran.a
 else
-export BLAS_LIB		?= -lblas
+export BLAS_LIB		?= -lessl -lm -lxlf90_r -lxl -lxlomp_ser -lxlfmath -L/opt/ibmcmp/xlsmp/2.1/lib64 -L/opt/ibmcmp/xlf/13.1/lib64 -R/opt/ibmcmp/lib64
 endif
 
 export POSTCOMPILE_AR		?= ar
